6ec9f50c560bfef7d28b778823f97470d6b78898
Carissa Wood <carissawood@outlook.com>
2019-03-13 23:07:18 -0700

    Extract datasets to create one shared module

--- datasets.py
+++ datasets.py
@@ -1,14 +1,15 @@
 from math import pi, sqrt, cos, sin
-from random import randint, random
+from random import randint, random, seed
 from collections import namedtuple
 from operator import itemgetter
 from time import time
 
+import jarvis_march
 import graham_scan
 
-Point = namedtuple("Point", "x y")
-n_points = 100
 
+Point = namedtuple("Point", "x y")
+n_points = 10
 
 def benchmark(sizes=[10, 100, 1000, 10000, 100000]):
     """Created as a performance metric."""
@@ -18,17 +19,24 @@ def benchmark(sizes=[10, 100, 1000, 10000, 100000]):
 def square_data_set():
     """Input data for the Convex Hull program are coordinates of the points (random integers)
     uniformly distributed over a square-shaped interval."""
-    ch = graham_scan.ConvexHull()
+    graham = graham_scan.GrahamScan()
+    jarvis = jarvis_march.JarvisMarch()
+    seed(33) # use seed to generate repeatable random numbers
     for _ in range(n_points):
-        ch.add(Point(randint(-100, 100), randint(-100, 100)))
-    print("Convex Hull:", ch.get_hull_points())
-    ch.display()
-
+        point = (Point(randint(-100, 100), randint(-100, 100)))
+        graham.add(point)
+        jarvis.add(point)
+    print("Convex Hull:", graham.get_hull_points())
+    graham.display() 
+    print("Convex Hull:", jarvis.get_hull_points())
+    jarvis.display() 
+        
+        
 
 def circle_data_set():
     """Input data for the Convex Hull program are coordinates of the points (random integers)
     uniformly distributed inside a circle."""
-    ch = graham_scan.ConvexHull()
+    points = []
     # R = 5
     for _ in range(n_points):
         t = random() * 2 * pi
@@ -38,15 +46,13 @@ def circle_data_set():
             r = u
         # r = R * sqrt(random())
         xy = [r * cos(t), r * sin(t)]
-        ch.add(Point(xy[0], xy[1]))
-    print("Convex Hull:", ch.get_hull_points())
-    ch.display()
-
+        points.append(Point(xy[0], xy[1]))
+    return points
 
 def hull_data_set():
     """Input data for the Convex Hull program are coordinates of the points (random integers)
     distributed on the hull."""
-    ch = graham_scan.ConvexHull()
+    points = []
     h = 1
     k = 2
     r = 5
@@ -54,15 +60,13 @@ def hull_data_set():
         theta = random() * 2 * pi
         x = h + cos(theta) * r
         y = k + sin(theta) * r
-        ch.add(Point(x, y))
-    print("Convex Hull:", ch.get_hull_points())
-    ch.display()
-
+        points.append(Point(x, y))
+    return points
 
 def triangle_data_set():
     """Input data for the Convex Hull program are coordinates of the points (random integers)
     distributed inside a triangle."""
-    ch = graham_scan.ConvexHull()
+    points = []
     p1 = (1, 1)
     p2 = (2, 4)
     p3 = (5, 2)
@@ -72,6 +76,13 @@ def triangle_data_set():
             s * p1[0] + (t - s) * p2[0] + (1 - t) * p3[0],
             s * p1[1] + (t - s) * p2[1] + (1 - t) * p3[1],
         )
-        ch.add(Point(s, t))
-    print("Convex Hull:", ch.get_hull_points())
-    ch.display()
+        points.append(Point(s, t))
+    return points
+
+
+def main():
+    square_data_set()    
+    
+
+if __name__ == "__main__":
+    main()
