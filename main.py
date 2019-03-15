e1be3fd323c58b7ef71123b679a601d9be0f57d7
Carissa Allen <carissawood@outlook.com>
2019-03-15 15:05:08 -0700

    Refactor main to run all algorithms consecutively

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
 from time import time
 
-from datasets import square_data_set, circle_data_set, triangle_data_set, hull_data_set
+from datasets import (
+    square_data_set,
+    circle_data_set,
+    triangle_data_set,
+    hull_data_set
+)
 import graham_scan
 import jarvis_march
 
 
-def run_graham_scan(dataset, size, show_progress):
+def run_algorithms(dataset, size, show_progress):
     graham = graham_scan.GrahamScan()
-    if dataset == 1:
-        square_data_set(graham, size)
-    elif dataset == 2:
-        circle_data_set(graham, size)
-    elif dataset == 3:
-        triangle_data_set(graham, size)
-    elif dataset == 4:
-        hull_data_set(graham, size)
-    else:
-        square_data_set(graham, size)
-    print("Convex Hull:", graham.get_hull_points(show_progress))
-    graham.display()
-
-
-def run_jarvis_march(dataset, size, show_progress):
     jarvis = jarvis_march.JarvisMarch()
     if dataset == 1:
+        square_data_set(graham, size)
         square_data_set(jarvis, size)
     elif dataset == 2:
-        circle_data_set(jarvis, size)
+        points = circle_data_set(graham, size)
+        add_points(jarvis, points)
     elif dataset == 3:
-        triangle_data_set(jarvis, size)
+        points = triangle_data_set(graham, size)
+        add_points(jarvis, points)
     elif dataset == 4:
-        hull_data_set(jarvis, size)
+        points = hull_data_set(graham, size)
+        add_points(jarvis, points)
     else:
+        square_data_set(graham, size)
         square_data_set(jarvis, size)
+    print("Convex Hull:", graham.get_hull_points(show_progress))
     print("Convex Hull:", jarvis.get_hull_points(show_progress))
+    graham.display()
     jarvis.display()
+
+
+def add_points(algorithm, points):
+    for p in points:
+        algorithm.add(p)
 
 
 def main():
@@ -51,8 +53,10 @@ def main():
     hull_dataset = 4  # all points distributed on the hull
 
     # run the algorithms
-    run_graham_scan(normal_dataset, input_size, show_progress)
-    run_jarvis_march(normal_dataset, input_size, show_progress)
+    # run_algorithms(normal_dataset, input_size, show_progress)
+    # run_algorithms(circle_dataset, input_size, show_progress)
+    # run_algorithms(triangle_dataset, input_size, show_progress)
+    # run_algorithms(hull_dataset, input_size, show_progress)
 
 
 if __name__ == "__main__":
