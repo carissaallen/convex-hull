c3109c3b33e5f18d4fdfabed28e0d0ac2debea21
Carissa Allen <carissawood@outlook.com>
2019-03-15 16:48:56 -0700

    Refactor scatter plot module
    One scatter plot method will show the final display of the convex
    hull solution, and one scatter plot will show an animation of the
    construction of the convex hull.

--- scatter_plot.py
+++ scatter_plot.py
@@ -2,22 +2,12 @@ from matplotlib import pyplot as plot
 from random import randint as rand
 
 
-def seed(count, min=0, max=100000):
-    """Returns a list of random (x,y) coordinates to plot on the graph."""
-    return [(rand(min, max), rand(min, max)) for _ in range(count)]
-
-
-def display():
-    """Displays the scatter plot."""
-    plot.title("Convex Hull")
-    plot.show()
-
-
-def scatter_plot(coordinates, convex_hull=None):
+def scatter_plot(coordinates, convex_hull=None, color="m", title="Convex Hull"):
     """Given a list of (x,y) coordinates, creates a scatter plot.
     The convex_hull is the list of (x,y) coordinates that make up the convex hull."""
     xs, ys = zip(*coordinates)  # zip(*iterables): unzips into x and y coordinate lists
     plot.scatter(xs, ys, marker=".", linestyle="None")
+    plot.title(title)
 
     if convex_hull != None:
         for i in range(1, len(convex_hull) + 1):
@@ -25,5 +15,25 @@ def scatter_plot(coordinates, convex_hull=None):
                 i = 0  # wrap the boundary line around
             c0 = convex_hull[i - 1]
             c1 = convex_hull[i]
-            plot.plot((c0[0], c1[0]), (c0[1], c1[1]), "g")
-        display()
+            plot.plot((c0[0], c1[0]), (c0[1], c1[1]), color)
+        plot.show()
+
+
+def draw_scatter_plot(coordinates, convex_hull=None, color="m", title="Convex Hull"):
+    """Given a list of (x,y) coordinates, creates a scatter plot.
+    The convex_hull is the list of (x,y) coordinates that make up the convex hull."""
+    xs, ys = zip(*coordinates)  # zip(*iterables): unzips into x and y coordinate lists
+    plot.scatter(xs, ys, marker=".", linestyle="None")
+    plot.title(title)
+
+    plot.ion()
+    plot.draw()
+    if convex_hull != None:
+        for i in range(1, len(convex_hull) + 1):
+            if i == len(convex_hull):
+                i = 0  # wrap the boundary line around
+            c0 = convex_hull[i - 1]
+            c1 = convex_hull[i]
+            plot.plot((c0[0], c1[0]), (c0[1], c1[1]), color, linewidth=0.7)
+            plot.pause(0.1)
+    plot.show()
